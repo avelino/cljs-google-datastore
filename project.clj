509d93808bc198c1ad0bde10dcdc09ca59987665
Avelino <t@avelino.xxx>
2020-04-10 08:30:30 -0300

    set scm git
    Signed-off-by: Avelino <t@avelino.xxx>

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/avelino/cljs-google-datastore"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
+  :scm {:name "git" :url "https://github.com/avelino/cljs-google-datastore"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]]
   :npm-deps {"@google-cloud/datastore" "5.1.0"}
