4f88d3a82fb655c988c9dbc3f7f3d4afce5e14e6
Avelino <t@avelino.xxx>
2020-04-10 08:27:53 -0300

    upgrade project description clojars description
    Signed-off-by: Avelino <t@avelino.xxx>

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljs-google-datastore "1.0.0"
-  :description "Interoperability @google-cloud/datastore to cljs"
+  :description "Google Cloud Datastore client to ClojureScript"
   :url "https://github.com/avelino/cljs-google-datastore"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
